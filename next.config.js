85c4a58e6efcc216a2578ab1383b98fe84ddbe6e
chenhaos112312 <braylal51703@gmail.com>
2025-03-16 11:15:55 +0800

    f

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('next').NextConfig} */
+
+const { withPlaiceholder } = require("@plaiceholder/next");
+const withPWA = require('next-pwa')({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+})
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        })
+        return config
+    },
+    images: {
+        domains: ['static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*.amazonaws.com',
+            },
+        ],
+    },
+}
+
+
+module.exports =
+    withPWA(
+        withPlaiceholder(
+            nextConfig
+        )
+    );
